ed5eaaa82b48d0769c4524f055e13288988be392
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2019-09-12 10:23:40 +0200

    TE17A grunder objektorientering

---
+++ TE17A_Klasser_Instanser/TE17A_Klasser_Instanser/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE17A_Klasser_Instanser
+{
+    class Enemy
+    {
+        private int hp = 10;
+        public string name = "";
+
+        public Enemy ()
+        {
+            Console.WriteLine("yay!");
+        }
+        
+        public void Hurt()
+        {
+            hp--;
+        }
+
+        public void PrintHp()
+        {
+            Console.WriteLine(hp);
+        }
+    }
+}
---
+++ TE17A_Klasser_Instanser/TE17A_Klasser_Instanser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE17A_Klasser_Instanser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy firstEnemy = new Enemy();
+            Enemy secondEnemy = new Enemy();
+            
+
+            firstEnemy.Hurt();
+
+            firstEnemy.PrintHp();
+
+            Console.ReadLine();
+
+        }
+    }
+}
