a26607b257102a9fd32f7edbcd89d3259ca7c080
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2019-11-06 10:59:32 +0100

    TE17A properties

---
+++ TE17A Properties/TE17A Properties/Hero.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE17A_Properties
+{
+    class Hero
+    {
+        private int xp = 0;
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string SurName { get; set; }
+
+        public int Xp
+        {
+            get
+            {
+                return xp;
+            }
+            set
+            {
+                xp = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return 1 + xp / 10;
+            }
+            private set
+            {
+
+            }
+        }
+
+
+        public void AddXp(int amount)
+        {
+            xp += amount;
+        }
+
+        public int GetXp()
+        {
+            return xp;
+        }
+
+    }
+}
---
+++ TE17A Properties/TE17A Properties/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE17A_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero h = new Hero();
+
+            h.Xp = 100;
+
+            h.Xp = "hej";
+
+            h.Name = "Hubert";
+
+            Console.WriteLine(h.Name);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
